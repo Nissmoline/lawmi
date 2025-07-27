bb6b8681613acd4bcb3535c4271de82afa93e42d
Konstantinos <zeus_kosta@hotmail.com>
2025-07-27 22:32:02 +0300

    wrangler fix

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          utils: ['react-router-dom']
+          router: ['react-router-dom']
         }
       }
     },
